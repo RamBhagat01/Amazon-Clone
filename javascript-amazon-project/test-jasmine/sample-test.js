4514e49264bfa1596cd3bdaf1737a67e9426dfbc
RamBhagat01 <sonirambhagat@gmail.com>
2025-07-06 14:53:22 +0530

    created 3 tests inside a suite

--- javascript-amazon-project/test-jasmine/sample-test.js
+++ javascript-amazon-project/test-jasmine/sample-test.js
@@ -4,8 +4,15 @@ describe('test suite: currency', ()=> {
 
     it('convert cent to dollars',()=> {
         expect(formatcurrency(2095)).toEqual('20.95')
-    })
+    });
 
+    it ('check for zero', () => {
+        expect(formatcurrency(0)).toEqual('0.00')
+    });
+
+    it('check for decimals', () => {
+        expect(formatcurrency(2000.5)).toEqual('20.01')
+    });
 
 
 });
