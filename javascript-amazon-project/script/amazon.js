2c4eca35fe870b24abed048b4e15815a41e0bf7f
RamBhagat01 <sonirambhagat@gmail.com>
2025-07-25 17:00:39 +0530

    finished

--- javascript-amazon-project/script/amazon.js
+++ javascript-amazon-project/script/amazon.js
@@ -72,20 +72,7 @@ function rendercallback() {
               ${product.priceCents1()}
             </div>
 
-            <div class="product-quantity-container">
-              <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
+            
 
             ${product.sizehtml()}
             ${product.warranty1()}
@@ -131,6 +118,8 @@ function rendercallback() {
 
       addtoCart(productId);
       updateCart();
+
+      
     }
     );
   }
